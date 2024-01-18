72cafceb89b9438002eaedfc40001ffb221eb903
Fazil Nabiyev <fazil.nabioglu@gmail.com>
2024-01-18 11:38:52 +0100

    vagrant file for deploying ubuntu vm to virtualbox

---
+++ vagrant/ubuntu/vagrant/setup-hosts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+IFNAME=$1
+ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
+sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
+
+# remove ubuntu-bionic entry
+sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
+
+# Update /etc/hosts about other hosts
+cat >> /etc/hosts <<EOF
+192.168.56.110  master-1
+#192.168.56.111  master-2
+#192.168.56.121  worker-1
+#192.168.56.122  worker-2
+#192.168.56.130  lb
+EOF
